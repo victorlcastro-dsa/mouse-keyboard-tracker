22eb8ee25501d96f478b5b8aaf34952fadd4f9f9
Victor Lopes de Castro <victorlcastro.dsa@gmail.com>
2024-12-30 10:39:26 -0300

    Logging Update

--- main.py
+++ main.py
@@ -2,8 +2,17 @@ from monitoramento.eventos import MonitorEventos
 from monitoramento.ociosidade import MonitorOciosidade
 from threading import Thread
 import time
+import logging
 
 def main():
+    logger = logging.getLogger('monitoramento')
+    handler = logging.FileHandler('monitoramento.log')
+    handler.setFormatter(logging.Formatter('%(asctime)s - %(message)s'))
+    logger.addHandler(handler)
+    logger.setLevel(logging.INFO)
+
+    logger.info("Monitoramento iniciado.")
+
     monitor_eventos = MonitorEventos()
     monitor_ociosidade = MonitorOciosidade(tempo_maximo_ocioso=10)
 
@@ -16,6 +25,7 @@ def main():
         while True:
             time.sleep(10)
     except KeyboardInterrupt:
+        logger.info("Monitoramento encerrado.")
         print("Monitoramento encerrado.")
 
 if __name__ == "__main__":
--- monitoramento/ociosidade.py
+++ monitoramento/ociosidade.py
@@ -14,15 +14,23 @@ class MonitorOciosidade:
             self.tempo_maximo_ocioso = tempo_maximo_ocioso
             self.ultimo_evento = time.time()
             self.ociosidade_logger = EventoLogger('ociosidade_eventos.log')
+            self.ocioso = False
             self.initialized = True
 
     def atualizar_ultimo_evento(self):
         self.ultimo_evento = time.time()
+        self.ocioso = False
 
     def verificar_ociosidade(self):
         while True:
             tempo_atual = time.time()
-            if tempo_atual - self.ultimo_evento > self.tempo_maximo_ocioso:
-                evento = f"O usuário está ocioso por mais de {self.tempo_maximo_ocioso} segundos."
-                self.ociosidade_logger.registrar_evento(evento)
+            tempo_ocioso = tempo_atual - self.ultimo_evento
+            if tempo_ocioso > self.tempo_maximo_ocioso:
+                if not self.ocioso:
+                    self.ocioso = True
+                    evento = f"O usuário está ocioso desde {time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(self.ultimo_evento))}."
+                    self.ociosidade_logger.registrar_evento(evento)
+                else:
+                    evento = f"O usuário está ocioso há {int(tempo_ocioso)} segundos."
+                    self.ociosidade_logger.registrar_evento(evento)
             time.sleep(1)
